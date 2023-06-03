76cb8187eb74c060ffffa22daea9c03bffb195d8
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-06-04 00:50:03 +0200

    be680 working

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,59 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define SEALEVELPRESSURE_HPA (1013.25)
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+Adafruit_BME680 bme; // I2C
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    ; // for Leonardo/Micro/Zero
+
+  if (!bme.begin(0x77)) // Change address here to 0x77
+  {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    while (1)
+      ;
+  }
+
+  // Set up oversampling and filter initialization
+  bme.setTemperatureOversampling(BME680_OS_8X);
+  bme.setHumidityOversampling(BME680_OS_2X);
+  bme.setPressureOversampling(BME680_OS_4X);
+  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+  bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  if (!bme.performReading())
+  {
+    Serial.println("Failed to perform reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bme.temperature);
+  Serial.println(" *C");
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Humidity = ");
+  Serial.print(bme.humidity);
+  Serial.println(" %");
+
+  Serial.print("Pressure = ");
+  Serial.print(bme.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Gas = ");
+  Serial.print(bme.gas_resistance / 1000.0);
+  Serial.println(" KOhms");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+  delay(2000);
 }
