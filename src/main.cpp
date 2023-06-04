80937d46d6a001042dab1ee04e8a33da0b46b089
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-06-04 02:10:06 +0200

    bme280

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,72 @@
+/*********
+  Complete project details at http://randomnerdtutorials.com
+*********/
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME680.h>
+#include <Adafruit_BME280.h>
+
+/*#include <SPI.h>
+#define BME_SCK 18
+#define BME_MISO 19
+#define BME_MOSI 23
+#define BME_CS 5*/
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-Adafruit_BME680 bme; // I2C
+Adafruit_BME280 bme; // I2C
+// Adafruit_BME280 bme(BME_CS); // hardware SPI
+// Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+
+unsigned long delayTime;
 
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial)
-    ; // for Leonardo/Micro/Zero
+  Serial.println(F("BME280 test"));
 
-  if (!bme.begin(0x77)) // Change address here to 0x77
+  bool status;
+
+  // default settings
+  // (you can also pass in a Wire library object like &Wire2)
+  status = bme.begin(0x76);
+  if (!status)
   {
-    Serial.println("Could not find a valid BME680 sensor, check wiring!");
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1)
       ;
   }
 
-  // Set up oversampling and filter initialization
-  bme.setTemperatureOversampling(BME680_OS_8X);
-  bme.setHumidityOversampling(BME680_OS_2X);
-  bme.setPressureOversampling(BME680_OS_4X);
-  bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150); // 320*C for 150 ms
+  Serial.println("-- Default Test --");
+  delayTime = 1000;
+
+  Serial.println();
 }
 
 void loop()
 {
-  if (!bme.performReading())
-  {
-    Serial.println("Failed to perform reading :(");
-    return;
-  }
   Serial.print("Temperature = ");
-  Serial.print(bme.temperature);
+  Serial.print(bme.readTemperature());
   Serial.println(" *C");
 
-  Serial.print("Humidity = ");
-  Serial.print(bme.humidity);
-  Serial.println(" %");
+  // Convert temperature to Fahrenheit
+  /*Serial.print("Temperature = ");
+  Serial.print(1.8 * bme.readTemperature() + 32);
+  Serial.println(" *F");*/
 
   Serial.print("Pressure = ");
-  Serial.print(bme.pressure / 100.0);
+  Serial.print(bme.readPressure() / 100.0F);
   Serial.println(" hPa");
-
-  Serial.print("Gas = ");
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(" KOhms");
 
   Serial.print("Approx. Altitude = ");
   Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
   Serial.println(" m");
 
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
   Serial.println();
-  delay(2000);
+
+  delay(delayTime);
 }
