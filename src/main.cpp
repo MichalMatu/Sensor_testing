0407892f78f2d2ec07654c1747f56d514fc44d5b
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-08-09 12:02:52 +0200

    acs712

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,27 @@
-#include <SPI.h>
-#include <SD.h>
-
-// Chip select pin for the SD card module
-const int chipSelect = 6;
-
+#include <Arduino.h>
 void setup()
 {
-  Serial.begin(115200);
-
-  // Initialize SD card
-  if (!SD.begin(chipSelect))
-  {
-    Serial.println("Card Mount Failed");
-    return;
-  }
-  Serial.println("Card Initialized");
-
-  // Create and open a file on the SD card
-  File dataFile = SD.open("/data.txt", FILE_WRITE);
-
-  // If the file is available, write to it
-  if (dataFile)
-  {
-    dataFile.println("Hello, this is data being written to the SD card!");
-    dataFile.close();
-    Serial.println("Data written to the file.");
-  }
-  else
-  {
-    Serial.println("Error opening file.");
-  }
+  Serial.begin(115200);     // Set serial monitor speed to 115200 baud
+  analogReadResolution(12); // Set ADC resolution to 12 bits (0-4095)
 }
 
 void loop()
 {
-  // Nothing to do here
+  int sensorValue = analogRead(34); // Read from pin D34
+
+  float voltage = sensorValue * (3.3 / 4095.0); // Convert ADC value to voltage
+  float current = (voltage - 1.65) / 0.066;     // Calculate current based on ACS712 sensitivity
+
+  Serial.print("Voltage: ");
+  Serial.print(voltage, 2); // Print voltage with 2 decimal places
+  Serial.print(" V\t");
+
+  Serial.print("Current: ");
+  Serial.print(current, 2); // Print current with 2 decimal places
+  Serial.println(" A");
+
+  Serial.print("ADC value: ");
+  Serial.println(sensorValue);
+
+  delay(1000); // Delay for 1 second
 }
